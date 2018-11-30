518406318ec79aa7b99d8970be4c25134e5bbc53
kekelele <550246366@qq.com>
2018-11-30 09:38:42 +0800

    Update 1x_Test.py
    fix problem

--- 1x_Test.py
+++ 1x_Test.py
@@ -1,34 +1,21 @@
 #coding:utf-8
-import J2534
+from J2534 import j2534lib
+from J2534 import wrapper as func
+from J2534.Define import *
+from J2534.Error import printerr
 import sys
-print J2534.j2534lib.getDeviceList()
+print(j2534lib.getDevices())
 
 
-J2534.j2534lib.setDevice(int(sys.argv[1]))
-id1 = J2534.ptOpen()
-id2 = J2534.ptOpen()
-id3 = J2534.ptOpen()
-print id1, id2, id3
-print J2534.ptReadVersion(id1)
-print J2534.ptReadVersion(id2)
-print J2534.ptReadVersion(id3)
-ch1 = J2534.ptConnect(id1, J2534.ISO15765, 0, 500000)
-ch2 = J2534.ptConnect(id1, J2534.ISO15765, 0, 500000)
-ch3 = J2534.ptConnect(id1, J2534.ISO15765, 0, 500000)
-ch4 = J2534.ptConnect(id1, J2534.ISO15765, 0, 500000)
-ch5 = J2534.ptConnect(id1, J2534.ISO15765, 0, 500000)
-print ch1, ch2, ch3,ch4,ch5
+j2534lib.setDevice(key=int(sys.argv[1]))
+ret, id1 = func.ptOpen()
 
-ch1 = J2534.ptConnect(id2, J2534.ISO15765, 0, 500000)
-ch2 = J2534.ptConnect(id2, J2534.ISO15765, 0, 500000)
-ch3 = J2534.ptConnect(id2, J2534.ISO15765, 0, 500000)
-ch4 = J2534.ptConnect(id2, J2534.ISO15765, 0, 500000)
-print ch1, ch2, ch3, ch4
-
-J2534.ptDisconnect(ch1)
-J2534.ptDisconnect(ch2)
-J2534.ptDisconnect(ch3)
-J2534.ptDisconnect(ch4)
-J2534.ptClose(id1)
-J2534.ptClose(id2)
-J2534.ptClose(id3)
+print(id1)
+ret, FirmwareVersion, DllVersion, ApiVersion = func.ptReadVersion(id1)
+print("FirmwareVersion : %s"%FirmwareVersion)
+print("DllVersion : %s"%DllVersion)
+print("ApiVersion : %s"%ApiVersion)
+ret, ch1 = func.ptConnect(id1, ProtocolID.CAN, 0, 500000)
+print(ch1)
+func.ptDisconnect(ch1)
+func.ptClose(id1)
